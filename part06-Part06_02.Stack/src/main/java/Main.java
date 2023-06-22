d0f670f0bfc60ba7bea77e77403087c21611cb74
Seremba Patrick <pserembae@gmail.com>
2023-06-22 15:18:16 +0300

    Stack challenge

--- part06-Part06_02.Stack/src/main/java/Main.java
+++ part06-Part06_02.Stack/src/main/java/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
-
+        Stack s = new Stack();
+        System.out.println(s.isEmpty());
+        System.out.println(s.values());
+        s.add("Value");
+        System.out.println(s.isEmpty());
+        System.out.println(s.values());
     }
 }
---
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author seremba
+ */
+public class Stack {
+
+    private ArrayList<String> stack;
+
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+
+    public void add(String value) {
+        this.stack.add(value);
+    }
+
+    public ArrayList<String> values() {
+        return stack;
+    }
+
+    public String take() {
+        return this.stack.remove(stack.size() - 1);
+    }
+}
