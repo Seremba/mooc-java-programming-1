7948d35f4a640374bb96860b56d509386e0e4609
Seremba Patrick <pserembae@gmail.com>
2023-06-22 16:13:12 +0300

    PrintingCollection

--- part06-Part06_04.PrintingACollection/src/main/java/Main.java
+++ part06-Part06_04.PrintingACollection/src/main/java/Main.java
@@ -4,24 +4,23 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
-        SimpleCollection j = new SimpleCollection("characters");
-        System.out.println(j);        
-        
+        SimpleCollection s = new SimpleCollection("alphabet");
+        System.out.println(s);
+
         System.out.println();
-        
-        j.add("magneto");
-        System.out.println(j);
-        
+
+        s.add("a");
+        System.out.println(s);
+
         System.out.println();
-        
-        j.add("mystique");
-        System.out.println(j);
-        
+
+        s.add("b");
+        System.out.println(s);
+
         System.out.println();
-        
-        j.add("phoenix");
-        System.out.println(j);
-        
-        
+
+        s.add("c");
+        System.out.println(s);
+
     }
 }
--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,24 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    public String toString() {
+        if (this.elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        }
+
+        String value = "";
+        if (this.elements.size() == 1) {
+            value = value + this.elements.get(0);
+            return "The collection " + this.name + " has " + this.elements.size() + " element:\n" + value;
+            //The collection alphabet has 1 element:
+        } else {
+            for (String element : elements) {
+                value = value + element + "\n";
+            }
+            return "The collection " + this.name + " has " + this.elements.size() + " elements:\n" + value;
+        }
+
+    }
+
 }
