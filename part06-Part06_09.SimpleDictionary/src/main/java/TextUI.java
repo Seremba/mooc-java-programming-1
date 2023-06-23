5d7e60a71d5c555fda0ea78888d4c505ccb03185
Seremba Patrick <pserembae@gmail.com>
2023-06-23 12:17:14 +0300

    create TextUI class

--- part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author seremba
  */
 public class TextUI {
+
     private Scanner scanner;
     private SimpleDictionary dictionary;
 
@@ -19,23 +19,39 @@ public class TextUI {
         this.scanner = scanner;
         this.dictionary = dictionary;
     }
-    public void start(){
-       
-        while(true){
-           System.out.println("Command: ");
-           String command = scanner.nextLine();
-           if(command.equals("end")){
-               System.out.println("Bye bye!");
-               break;
-           }
-            System.out.println("Unknown command");
-            if(command.equals("add")){
+
+    public void start() {
+
+        while (true) {
+            System.out.println("Command: ");
+            String command = scanner.nextLine();
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            } else if (command.equals("add")) {
                 System.out.println("Word:");
                 String word = scanner.nextLine();
                 System.out.println("Translation:");
                 String translation = scanner.nextLine();
                 dictionary.add(word, translation);
+            } else if (command.equals("search")) {
+                System.out.println("To be translated:");
+                String word = scanner.nextLine();
+                if (this.dictionary.translate(word) == null) {
+                    System.out.println("Word " + word + " was not found");
+                } else {
+                    System.out.println("Translation:");
+
+                    System.out.println(dictionary.translate(word));
+                }
+
+            } else {
+
+                System.out.println("Unknown command");
+
             }
+//            
         }
+
     }
 }
