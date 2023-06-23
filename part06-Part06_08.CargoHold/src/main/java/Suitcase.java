b4544cc8f9f30a40c522c7cf96f8bf523a49c420
Seremba Patrick <pserembae@gmail.com>
2023-06-23 10:16:21 +0300

    add suitcase toString() method

--- part06-Part06_08.CargoHold/src/main/java/Suitcase.java
+++ part06-Part06_08.CargoHold/src/main/java/Suitcase.java
@@ -36,18 +36,19 @@ public class Suitcase {
         return totalWeight;
     }
 
-    public Item heaviestItem(){
-        if(this.items.isEmpty()){
+    public Item heaviestItem() {
+        if (this.items.isEmpty()) {
             return null;
         }
         Item heaviest = this.items.get(0);
-        for(Item item: this.items){
-            if(heaviest.getWeight() < item.getWeight()){
+        for (Item item : this.items) {
+            if (heaviest.getWeight() < item.getWeight()) {
                 heaviest = item;
             }
         }
         return heaviest;
     }
+
     public void printItems() {
         for (Item item : this.items) {
             System.out.println(item);
@@ -56,14 +57,12 @@ public class Suitcase {
 
     public String toString() {
         if (this.items.size() == 0) {
-            return "0 items (0 kg)";
-        }
-        String i = "";
-        if (this.items.size() == 1) {
-            i = this.items.size() + " item (" + this.items.get(0).getWeight() + "kg)";
+            return "no items (0 kg)";
+        } else if (this.items.size() == 1) {
+            return "1 item (" + totalWeight() + " kg)";
         } else {
-            i = this.items.size() + " items (" + totalWeight() + "kg)";
+            return this.items.size() + " items (" + totalWeight() + " kg)";
         }
-        return i;
+
     }
 }
