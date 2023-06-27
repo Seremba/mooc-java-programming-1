f3eb20031dd83af16a4470d9a9c2b72c891df316
Seremba Patrick <pserembae@gmail.com>
2023-06-27 16:50:15 +0300

    create Container class

--- part07-Part07_02.LiquidContainers2/src/main/java/Container.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/Container.java
@@ -1,39 +1,40 @@
-
-import java.util.ArrayList;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author seremba
  */
 public class Container {
+
     private int liquid;
 
     public Container() {
         this.liquid = 0;
     }
+
     public int contains() {
         return this.liquid;
     }
+
     public void add(int amount) {
-        if(amount >= 0 && amount + liquid <= 100){
+        if (amount >= 0 && amount + liquid <= 100) {
             this.liquid += amount;
         } else {
             this.liquid = 100;
         }
     }
-    public void remove(int amount){
-        if(liquid - amount >= 0 && liquid - amount <= 100){
+
+    public void remove(int amount) {
+        if (liquid - amount >= 0 && liquid - amount <= 100) {
             this.liquid -= amount;
         } else {
             this.liquid = 0;
         }
     }
+
     public String toString() {
         return this.liquid + "/100";
     }
