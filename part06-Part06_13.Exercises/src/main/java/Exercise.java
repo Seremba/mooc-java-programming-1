374b29f97cb31174d19488394396fbca7dba2cb6
Seremba Patrick <pserembae@gmail.com>
2023-06-27 11:58:01 +0300

    Exercise challenge

---
+++ part06-Part06_13.Exercises/src/main/java/Exercise.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author seremba
+ */
+    
+public class Exercise {
+
+    private String name;
+    private boolean completed;
+
+    public Exercise(String exerciseName) {
+        this.name = exerciseName;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
