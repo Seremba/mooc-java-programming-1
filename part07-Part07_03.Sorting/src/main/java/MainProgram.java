b235c6830863e4d1ab621baf4acffa06c4b8b8c4
Seremba Patrick <pserembae@gmail.com>
2023-06-29 14:43:48 +0300

    swap numbers

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,15 +1,21 @@
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
         // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
-        int smallest = smallest(array);
-        System.out.println(smallest);
-        
-        System.out.println("");
-        int index = indexOfSmallest(array);
-        System.out.println(index);
+
+        int[] nums = {3, 2, 5, 4, 8};
+
+        System.out.println(Arrays.toString(nums));
+
+        MainProgram.swap(nums, 1, 0);
+        System.out.println(Arrays.toString(nums));
+
+        MainProgram.swap(nums, 0, 3);
+        System.out.println(Arrays.toString(nums));
+
     }
 
     public static int smallest(int[] array) {
@@ -36,5 +42,27 @@ public class MainProgram {
             }
         }
         return index;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        // write your code here
+        int index = 0;
+        int smallest = table[startIndex];
+        for (int i = startIndex; i < table.length; i++) {
+            if (smallest >= table[i]) {
+                smallest = table[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        // write your code here
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        
     }
 }
