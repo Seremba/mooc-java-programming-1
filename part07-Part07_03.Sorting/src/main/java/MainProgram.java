134d6b183a674580d287af9f01abf842c274f0a0
Seremba Patrick <pserembae@gmail.com>
2023-06-29 11:34:19 +0300

    Index of smallest

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -6,6 +6,10 @@ public class MainProgram {
         int[] array = {3, 1, 5, 99, 3, 12};
         int smallest = smallest(array);
         System.out.println(smallest);
+        
+        System.out.println("");
+        int index = indexOfSmallest(array);
+        System.out.println(index);
     }
 
     public static int smallest(int[] array) {
@@ -19,5 +23,18 @@ public class MainProgram {
         }
         return smallest;
 
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        // write your code here
+        int smallest = array[0];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (smallest > array[i]) {
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
     }
 }
