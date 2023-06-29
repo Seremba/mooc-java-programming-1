17be4079e011af01bc80dd26bac9174637351703
Seremba Patrick <pserembae@gmail.com>
2023-06-29 19:14:50 +0300

    test program

--- part07-Part07_06.GradeStatistics/src/main/java/Main.java
+++ part07-Part07_06.GradeStatistics/src/main/java/Main.java
@@ -7,33 +7,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here -- consider breaking the program into 
-        // multiple classes.
-        int total = 0;
-        int sum = 0;
-        double passing = 0;
-        double avg = 0;
-        System.out.println("Enter point totals, -1 stops:");
-        while (true) {
-            int points = Integer.valueOf(scanner.nextLine());
-            if (points == -1) {
-                break;
-            }
-            if (points >= 0 && points <= 100) {
-                sum += points;
-                total += 1;
-                avg = 1.0 * sum / total;
-            }
-            if (points >= 0 && points <= 100) {
-                if (points >= 50) {
-                    sum += points;
-                    total += 1;
-                    passing = 1.0 * sum / total;
-                } else {
-                    System.out.println("Point average (passing): -");
-                }
-            }
-        }
-        System.out.println("Point average (all): " + avg);
-
+        // multiple classe
+        Statistics statistics = new Statistics();
+        
+        UserInterface ui = new UserInterface(statistics, scanner);
+        ui.start();
     }
 }
