13449e9275d0fcf19694a022fc5e583e2b1e2dad
Seremba Patrick <pserembae@gmail.com>
2023-07-02 13:15:11 +0300

    add ui to the program

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,10 @@
  * @author seremba
  */
 public class RecipeSearch {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        RecipeController recipeController = new RecipeController();
+        UserInterface ui = new UserInterface(scanner, recipeController);
+        ui.start();
+    }
 }
