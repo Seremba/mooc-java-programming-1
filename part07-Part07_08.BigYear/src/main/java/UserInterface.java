c4b4dbc47a829f05c7cd78f2659ea9f36e29b3d1
Seremba Patrick <pserembae@gmail.com>
2023-07-03 13:59:14 +0300

    some changes in the UserInterface class

--- part07-Part07_08.BigYear/src/main/java/UserInterface.java
+++ part07-Part07_08.BigYear/src/main/java/UserInterface.java
@@ -11,44 +11,37 @@ import java.util.Scanner;
  * @author seremba
  */
 public class UserInterface {
-
+    
     private Scanner scanner;
     private BirdDatabase birdDatabase;
-
+    
     public UserInterface(Scanner scanner, BirdDatabase birdDatabase) {
         this.scanner = scanner;
         this.birdDatabase = birdDatabase;
     }
-
+    
     public void start() {
         while (true) {
             System.out.print("? ");
             String command = scanner.nextLine();
-            if (command.equals("quit")) {
+            if (command.equals("Quit")) {
                 break;
             }
-
-            if (command.equals("add")) {
-                System.out.print("Name: ");
-                String name = scanner.nextLine();
-                System.out.print("Name in Latin: ");
-                String latin = scanner.nextLine();
-                this.birdDatabase.add(name, latin);
+            
+            if (command.equals("Add")) {
+                this.birdDatabase.addBird(scanner);
             } else if (command.equals("Observation")) {
-                System.out.print("Bird? ");
-                String name = scanner.nextLine();
-                this.birdDatabase.observation(name);
-            } else if (command.equals("all")) {
-                this.birdDatabase.printBirdsInTheDatabse();
-            } else if (command.equals("one")) {
-                System.out.println("in printOneBird");
-                System.out.print("Bird? ");
-                String birdName = scanner.nextLine();
-                this.birdDatabase.printDatabaseBird(birdName);
+                
+                this.birdDatabase.observeBird(scanner);
+            } else if (command.equals("All")) {
+                this.birdDatabase.retrieveAllBirds();
+            } else if (command.equals("One")) {
+                
+                this.birdDatabase.retrieveOneBird(scanner);
             } else {
                 System.out.println("Not a valid command!!");
             }
         }
     }
-
+    
 }
